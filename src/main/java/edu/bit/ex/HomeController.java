687c05d4b5b0312c18156f6b517e9215687b1e7d
seulpi <nwey5042@gmail.com>
2021-02-25 10:57:56 +0900

    no message

--- src/main/java/edu/bit/ex/HomeController.java
+++ src/main/java/edu/bit/ex/HomeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
-// 수정1
+// #1
 @Controller
 public class HomeController {
 	
